764a6a0ac415132a0faa9f3039fd99ab8d4b99a8
github-action-benchmark <github@users.noreply.github.com>
2025-10-03 19:15:02 +0000

    add Benchmark (pytest) benchmark result for 78c95203e8981f65fc6b87f710011826898aa173

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759518684677,
+  "lastUpdate": 1759518902858,
   "repoUrl": "https://github.com/egidioln/QuaTorch",
   "entries": {
     "Benchmark": [
@@ -90,6 +90,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.005135780141267258",
             "extra": "mean: 257.15357139999924 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lucasegidio1@gmail.com",
+            "name": "Lucas Egidio",
+            "username": "egidioln"
+          },
+          "committer": {
+            "email": "lucasegidio1@gmail.com",
+            "name": "Lucas Egidio",
+            "username": "egidioln"
+          },
+          "distinct": true,
+          "id": "78c95203e8981f65fc6b87f710011826898aa173",
+          "message": "ci: download artifacts in job deploy",
+          "timestamp": "2025-10-03T21:13:32+02:00",
+          "tree_id": "652df74b6f953d4afd30b425a079222b2620f9c1",
+          "url": "https://github.com/egidioln/QuaTorch/commit/78c95203e8981f65fc6b87f710011826898aa173"
+        },
+        "date": 1759518901856,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "test/benchmark/test_performance.py::test_performance_slerp",
+            "value": 0.6322312326030525,
+            "unit": "iter/sec",
+            "range": "stddev: 0.01341524449359763",
+            "extra": "mean: 1.5816997775999653 sec\nrounds: 5"
+          },
+          {
+            "name": "test/benchmark/test_performance.py::test_performance_rotate_vector",
+            "value": 1.434203052230726,
+            "unit": "iter/sec",
+            "range": "stddev: 0.006273776337408327",
+            "extra": "mean: 697.2513400000253 msec\nrounds: 5"
+          },
+          {
+            "name": "test/benchmark/test_performance.py::test_performance_multiplication",
+            "value": 3.6082341707894297,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0027453925631609954",
+            "extra": "mean: 277.1438750000016 msec\nrounds: 5"
           }
         ]
       }
