8e54ff8c2b91e1a30c929ba954b2e14842500921
Lucas Egidio <lucasegidio1@gmail.com>
2026-01-30 01:28:11 +0100

    test: check if mul is torch compilable

--- test/benchmark/test_performance.py
+++ test/benchmark/test_performance.py
@@ -16,40 +16,71 @@ def test_performance_slerp(benchmark):
     t = torch.rand(2, 1, 1, device=DEVICE)
 
     def slerp():
-        x = Quaternion.slerp(q1, q2, t)[0, 0, 0].item()
+        x = Quaternion.slerp(q1, q2, t)[0, 0, 0]
+        torch.cuda.synchronize()
 
     result = benchmark(
         slerp,
     )
 
 
+@pytest.mark.parametrize("num_points", [1000000, 5000000, 10000000])
 @pytest.mark.benchmark(
     group=str(DEVICE),
     warmup=False,
 )
-def test_performance_rotate_vector(benchmark):
-    q1 = Quaternion(torch.randn(10000000, 4, device=DEVICE))
-    vectors = torch.randn(10000000, 3, device=DEVICE)
+def test_performance_rotate_vector(benchmark, num_points):
+    q1 = Quaternion(torch.randn(num_points, 4, device=DEVICE))
+    vectors = torch.randn(num_points, 3, device=DEVICE)
 
     def rotate_vector():
-        x = Quaternion.rotate_vector(q1, vectors)[0, 0].item()
+        x = Quaternion.rotate_vector(q1, vectors)
+        torch.cuda.synchronize()
 
     result = benchmark(
         rotate_vector,
     )
 
 
+def multiplication(q1: Quaternion, q2: Quaternion) -> Quaternion:
+    return q1 * q2
+
+
+multiplication_compiled = torch.compile(
+    multiplication,
+    fullgraph=True,
+    # mode="max-autotune",  # The execution time increases by 3x with this option
+)
+
+
+@pytest.mark.parametrize(
+    "multiplication",
+    [multiplication, multiplication_compiled],
+)
 @pytest.mark.benchmark(
     group=str(DEVICE),
     warmup=False,
 )
-def test_performance_multiplication(benchmark):
+def test_performance_multiplication(benchmark, multiplication):
     q1 = Quaternion(torch.randn(10000000, 4, device=DEVICE))
     q2 = Quaternion(torch.randn(10000000, 4, device=DEVICE))
 
-    def multiplication():
-        x = Quaternion.mul(q1, q2)[0, 0].item()
+    for warmup_n in range(2):
+        q = multiplication(q1, q2)
+        torch.cuda.synchronize()
 
+    torch.cuda.synchronize()
     result = benchmark(
         multiplication,
+        q1,
+        q2,
     )
+
+
+def test_compile_multiplication_match():
+    q1 = Quaternion(torch.randn(10000, 4, device=DEVICE))
+    q2 = Quaternion(torch.randn(10000, 4, device=DEVICE))
+    result_compiled = multiplication_compiled(q1, q2)
+    result = multiplication(q1, q2)
+
+    assert torch.allclose(result, result_compiled, atol=1e-6)
