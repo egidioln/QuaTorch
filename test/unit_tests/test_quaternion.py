test: fix missing cast to Quaternion (wrong kind of division made in test)

test/unit_tests/test_quaternion.py
@@ -357,7 +357,7 @@ def test_from_rotation_matrix_under_edge_case_when_R_is_symmetric_and_trace_is_n
     )
 
     # division between expected and computed should be close to 1 or -1, as these are the same quaternion.
-    expected_quaternion = torch.stack([q_0, q_1, q_2, q_3])
+    expected_quaternion = Quaternion(torch.stack([q_0, q_1, q_2, q_3]))
     assert torch.all(
         torch.all(
             torch.isclose(